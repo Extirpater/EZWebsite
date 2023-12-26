704118a76db1c44ebce2bc63be153bea67c9dc48
Extirpater <edwardbzhang@gmail.com>
2023-12-26 15:19:25 -0500

    added home page

---
+++ src/components/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { styles } from "../styles";
+const Home = () => {
+  return (
+    <div className="h-screen flex items-center justify-center">
+      <h1 className={`${styles.heroHeadText}`}>Edward Zhang</h1>
+    </div>
+  );
+};
+
+export default Home;
